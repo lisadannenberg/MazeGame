04c4f829ec4d87058617d462a91a5e2b150a3cf3
li_da <li_da@uni-bremen.de>
2018-11-05 16:52:44 +0100

    maze game part 2

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 	public float moveSpeed = 7.0f;
 	private float maxDistance = 0.0f;
+	private float damege = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	private GameObject triggerEnemy;
+
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 		maxDistance += 1 * Time.deltaTime;
 		if (maxDistance >= 5) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Enemey") {
+			triggerEnemy = other.gameObject;
+			triggerEnemy.GetComponent<Enemey>().health -= damege;
 			Destroy(this.gameObject);
 		}
 	}
---
+++ Assets/Scripts/Enemey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemey : MonoBehaviour {
+
+	//Members
+	public float health = 1.0f;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0) {
+			Die();
+		}
+
+
+	}
+	void Die()
+	{
+		print("Enemy dies" + this.gameObject.name);
+		Destroy(this.gameObject);
+	}
+}
